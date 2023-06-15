ba4bdb632a00b1de32f779a49de5918f9064f170
shaik Musthafa <musthafaskkrnk@gmail.com>
2023-06-15 16:44:54 +0530

    new project

---
+++ app.py
@@ -0,0 +1,53 @@
+import numpy as np
+import pandas as pd
+from flask import Flask, request, render_template
+import pickle
+
+app = Flask(__name__)
+model = pickle.load(open('model.pkl', 'rb'))
+
+dataset = pd.read_csv('diabetes.csv')
+
+dataset_X = dataset.iloc[:, [1, 2, 5, 7]].values
+
+from sklearn.preprocessing import MinMaxScaler
+
+sc = MinMaxScaler(feature_range=(0, 1))
+dataset_scaled = sc.fit_transform(dataset_X)
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    '''
+    For rendering results on HTML GUI
+    '''
+    float_features = [float(x) for x in request.form.values()]
+    final_features = [np.array(float_features)]
+    prediction = model.predict(sc.transform(final_features))
+
+    if prediction == 1:
+        pred = "You have Diabetes, please consult a Doctor."
+        suggestions = get_food_suggestions(1)
+    else:
+        pred = "You don't have Diabetes."
+        suggestions = get_food_suggestions(0)
+
+    return render_template('index.html', prediction_text=pred, suggestions_text=suggestions)
+
+
+def get_food_suggestions(prediction):
+    if prediction == 1:
+        suggestions = "'Food Item 4', 'Food Item 5', 'Food Item 6'"
+    else:
+        suggestions = "'Food Item 1', 'Food Item 2', 'Food Item 3'"
+
+    return suggestions
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ model.py
@@ -0,0 +1,102 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+import warnings
+warnings.filterwarnings('ignore')
+
+import pickle
+
+# In[2]:
+
+
+dataset = pd.read_csv('diabetes.csv')
+
+
+
+# # Step 3: Data Preprocessing
+
+# In[13]:
+
+
+dataset_X = dataset.iloc[:,[1, 4, 5, 7]].values
+dataset_Y = dataset.iloc[:,8].values
+
+
+# In[14]:
+
+
+dataset_X
+
+
+# In[15]:
+
+
+from sklearn.preprocessing import MinMaxScaler
+sc = MinMaxScaler(feature_range = (0,1))
+dataset_scaled = sc.fit_transform(dataset_X)
+
+
+# In[16]:
+
+
+dataset_scaled = pd.DataFrame(dataset_scaled)
+
+
+# In[17]:
+
+
+X = dataset_scaled
+Y = dataset_Y
+
+
+# In[18]:
+
+
+X
+
+
+# In[19]:
+
+
+Y
+
+
+# In[20]:
+
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.20, random_state = 42, stratify = dataset['Outcome'] )
+
+
+# # Step 4: Data Modelling
+
+# In[25]:
+
+
+from sklearn.svm import SVC
+svc = SVC(kernel = 'linear', random_state = 42)
+svc.fit(X_train, Y_train)
+
+
+# In[26]:
+
+
+svc.score(X_test, Y_test)
+
+
+# In[27]:
+
+
+Y_pred = svc.predict(X_test)
+
+
+
+
+
+pickle.dump(svc, open('model.pkl','wb'))
+model = pickle.load(open('model.pkl','rb'))
+#print(model.predict(sc.transform(np.array([[86, 66, 26.6, 31]]))))
+
+
